9c54d6e90ab2ae4fb80bbbe56aa633ec4095fbeb
MarioTechmade <124580204+MarioTechmade@users.noreply.github.com>
2023-02-06 20:07:55 +0100

    Add files via upload

---
+++ random passwd.py
@@ -0,0 +1,20 @@
+import random
+
+print('Password Generator NetworkMario')
+
+chars = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ1234567890!?@#+-_'
+
+number = input ('Quante password vuoi generare per hackerare Enzo il King?:')
+
+number = int(number)
+
+lenght = input('Quantità caratteri di password:')
+lenght = int(lenght)
+
+print('Queste sono le tue passwd:')
+
+for pwd in range (number):
+    passwords = ''
+    for c in range(lenght):
+        passwords += random.choice(chars)
+    print(passwords)
